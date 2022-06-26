1ffdce2a4cdfebb1347e82a045788797c57e3172
Arif Aydoğan <aydoganarif@yahoo.com>
2022-06-26 05:06:08 +0300

    Add project files.

---
+++ Project13-Barcode Generator/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BarcodeLib;
+using Project13_Barcode_Generator;
+
+Barcode barcode =Process.CreateAndSaveBarcode("777");
+Console.WriteLine(Process.ReadBarcode(barcode));
+
+barcode = Process.CreateAndSaveBarcode("11");
+Console.WriteLine(Process.ReadBarcode(barcode));
+
+barcode = Process.CreateAndSaveBarcode("111");
+Console.WriteLine(Process.ReadBarcode(barcode));
+
+barcode = Process.CreateAndSaveBarcode("1111");
+Console.WriteLine(Process.ReadBarcode(barcode));
